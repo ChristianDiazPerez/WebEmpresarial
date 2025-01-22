dac3620e4a9cdc69f67858823ea7f0ff45ef7483
Christian Díaz <christiandp99@gmail.com>
2025-01-22 21:43:56 +0100

    Añado navegador en el head

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+//Cabecera pegajosa
+var header = document.getElementById('Header')
+
+window.addEventListener("scroll", function(){
+    var scroll = window.scrollY;
+    if(scrollY>0){
+        header.style.backgroundColor = '#121212';
+
+    }
+    else{
+        header.style.backgroundColor = 'transparent';
+    }
+
+})
+
+//Ligh box
 const images = document.querySelectorAll("img[data-lightbox]");
 
 if (images) {
